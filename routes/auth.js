b12911ad49917eb3ec87f67556ac552fd4ce89f6
suphakamonLak <165622318+suphakamonLak@users.noreply.github.com>
2025-05-04 02:07:16 +0700

    middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 // step 1 import
 const express = require('express')
 const router = express.Router()
-const { register, login, currentUser } = require('../controllers/auth')
+const { register, login, currentUser } = require('../controllers/auth')// import controller
+const { authCheck, adminCheck } = require('../middleware/authCheck')
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('/current-user', currentUser)// ใช้ใน front-end เพื่อเช็ค authen ว่าล็อกอินหรือยัง
-router.post('/current-admin', currentUser)
+router.post('/current-user', authCheck, currentUser)// ใช้ใน front-end เพื่อเช็ค authen ว่าล็อกอินหรือยัง
+router.post('/current-admin', authCheck, adminCheck , currentUser)
 
 module.exports = router
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,11 @@
 const express  = require('express')
 const router = express.Router()
 const { create, list, remove } = require('../controllers/category')
+const { authCheck, adminCheck } = require('../middleware/authCheck')
 
 // @ENDPOINT http://localhost:5000/api/category
-router.get('/category', list)
-router.post('/category', create)
-router.delete('/category/:id', remove)
+router.get('/category', authCheck, adminCheck, list)
+router.post('/category', authCheck, adminCheck, create)
+router.delete('/category/:id', authCheck, adminCheck, remove)
 
 module.exports = router
