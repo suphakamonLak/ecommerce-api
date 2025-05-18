cd2a8e909aba0454667af6fda44e107f3b664162
suphakamonLak <165622318+suphakamonLak@users.noreply.github.com>
2025-05-18 19:36:02 +0700

    contents

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { authCheck } = require('../middleware/authCheck')
-const { changeOrderStatus, getOrderAdmin  } = require('../controllers/admin')
+const { changeOrderStatus, getOrderAdmin, getDashboard  } = require('../controllers/admin')
 
 router.put('/admin/order-status', authCheck, changeOrderStatus)
 router.get('/admin/orders', authCheck, getOrderAdmin)
+router.get('/admin/dashboard', authCheck, getDashboard)
 
 module.exports = router
