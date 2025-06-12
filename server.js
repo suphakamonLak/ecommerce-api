a0dedc553cca5365fa700f80cbd500f2687efefe
Suphakamon Lakkhasorn <165622318+suphakamonLak@users.noreply.github.com>
2025-06-12 13:10:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
-// step 1 import
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const { readdirSync } = require('fs')// ใช้เอามาอ่านไดเรกทอรี
-const cors = require('cors') // ให้ server กับ client ติดต่อกันได้
-
-// const authRouter = require('./routes/auth')
-// const categoryRouter = require('./routes/category')
+const { readdirSync } = require('fs')
+const cors = require('cors') 
 
 // middleware
 app.use(morgan('dev'))// dev, combined
 app.use(express.json({limit: '20mb'}))// อนุญาตให้ส่งข้อมูลขนาดใหญ่
 app.use(cors())// allow all
 
-// app.use('/api', authRouter)
-// app.use('/api', categoryRouter)
 readdirSync('./routes')
     .map((c) => app.use('/api', require('./routes/' + c)) 
 )
 
-// step 3 Router
-// app.post('/api', (req, res) => {
-//     // code
-//     const { username, password } = req.body// destructuring (แยกค่าออกจากออบเจกต์หรืออาร์เรย์)
-//     console.log(username, password)// access values 
-//     res.send('Jukkru')// ส่งออกไป
-// })
-
-// app.get('/', (req, res) => {
-//   res.send('API is running!');
-// });
 
 // step 2 Start server
 app.listen(5000, 
